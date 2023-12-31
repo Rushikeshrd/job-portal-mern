c53d1380090ab687ae38dc38131ede4171ab55d1
Rushikesh <rushikeshdasarwad23@gmail.com>
2023-12-31 14:02:44 +0530

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold text-indigo-700">Helloo World</h1>
+      <Navbar/>
+      <Outlet />
     </>
   );
 }
